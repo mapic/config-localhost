7c05c40d74529ef23a9da1ba3589a8e4f31f8bf8
knutole <knutolesjoli@gmail.com>
2016-10-17 10:49:39 +0200

    redacted keys

---
+++ localhost/pile-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+	// tileserver port
+	port : 3003,
+
+
+	redis : {
+
+		layers : {
+			port : 6379,
+			host : 'redislayers',
+			auth : 'MUST_MATCH_REDIS_TOKEN',
+			db : 2
+		},
+
+		stats : {
+			port : 6379,
+			host : 'redisstats',
+			auth : 'MUST_MATCH_REDIS_TOKEN',
+			db : 2
+		},
+
+		temp : {
+			port : 6379,
+ 			host : 'redistemp',
+   			auth : 'MUST_MATCH_REDIS_TOKEN',
+   			db : 2
+		}
+
+	},
+
+	// mongo
+	mongo : {
+		url : 'mongodb://mongo/systemapic'
+	},
+
+	path : {
+		log : '/data/logs/'
+	},
+
+	// no access
+	noAccessMessage : 'No access. Please contact Systemapic.com if you believe you are getting this message in error.',
+	noAccessTile    : 'public/noAccessTile.png',
+	
+	// waiting tile
+	processingTile : 'public/noAccessTile.png',
+
+	// stylesheets
+	defaultStylesheets : {
+		raster : 'public/cartoid.xml',
+		utfgrid : 'public/utfgrid.xml'
+	},
+
+}
